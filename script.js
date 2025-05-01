4304bcd3961c09518e9bef60e77c7be3368aecf8
Gheorghii Rotaru <gheorghiirotaru13@gmail.com>
2025-05-01 13:48:17 +0300

    Prima versiune

---
+++ script.js
@@ -0,0 +1,140 @@
+  const DATA = {
+    "Pneumologie": {
+      "1-50": "https://onlinetestpad.com/mks2ks52pmoha",
+      "51-100": "https://onlinetestpad.com/aqn7d6uzx64a6",
+      "101-150": "https://onlinetestpad.com/c6zemeat6a7mu",
+      "151-200": "https://onlinetestpad.com/llohvmxlso4b6",
+      "201-250": "https://onlinetestpad.com/inb4rqilvefm4",
+      "251-300": "https://onlinetestpad.com/cj3l3pvf4vuha",
+      "301++": "https://onlinetestpad.com/ok3b5ehisolt6"
+    },
+    "Cardiologie": {
+      "1-50": "https://onlinetestpad.com/k5g43dru2hud2",
+      "51-100": "https://onlinetestpad.com/q6yghcfve2l52",
+      "101-150": "https://onlinetestpad.com/nn2duuzikaoji",
+      "151-200": "https://onlinetestpad.com/kwfangsszprxs",
+      "201-250": "https://onlinetestpad.com/ekpolovvspdnq",
+      "251-300": "https://onlinetestpad.com/l75m5xvtdjxde",
+      "301-350": "https://onlinetestpad.com/sz3vvfzkxmnn6",
+      "351-400": "https://onlinetestpad.com/zwwg2kbqsxvqk",
+      "401-450": "https://onlinetestpad.com/c3e2ig5p53hay",
+      "451++": "https://onlinetestpad.com/m4x4wo2eh7tbc"
+    },
+    "Gastroenterologie": {
+      "1-50": "https://onlinetestpad.com/m73mt52ffwikk",
+      "51-100": "https://onlinetestpad.com/qwzsootwu3gsu",
+      "101-150": "https://onlinetestpad.com/fdj22sd5kwova",
+      "151-200": "https://onlinetestpad.com/pqxtyevcer6k2",
+      "201-250": "https://onlinetestpad.com/jgypi7sbq4ew6",
+      "251-300": "https://onlinetestpad.com/ba6rz4nqu6lwy",
+      "301-350": "https://onlinetestpad.com/bw2esbv22qpoe",
+      "351-400": "https://onlinetestpad.com/3hq7vtmbnhbw2",
+      "401++": "https://onlinetestpad.com/rcsejxkrjou4s"
+    },
+    "Reumatologie": {
+      "1-50": "https://onlinetestpad.com/pk3c2ix7jkva2",
+      "51-100": "https://onlinetestpad.com/lmywe2z7k2gfq",
+      "101-150": "https://onlinetestpad.com/a2ywqfe2cteja",
+      "151-200": "https://onlinetestpad.com/2tladmxupgpic",
+      "201++": "https://onlinetestpad.com/ocm4ercjqvrs2"
+    },
+    "Nefrologie": {
+      "1-50": "https://onlinetestpad.com/6yvxtshwotzo4",
+      "51-100": "https://onlinetestpad.com/zmq5oc5apphse",
+      "101-150": "https://onlinetestpad.com/iqdkqbi7ewaj",
+      "151-200": "https://onlinetestpad.com/63f4cn2nwve34",
+      "201++": "https://onlinetestpad.com/jvf74pbmgzxqm"
+    },
+    "Pediatrie": {
+      "1-50": "https://onlinetestpad.com/e7ma63xlasahq",
+      "51-100": "https://onlinetestpad.com/wlt2ynqflxwt2",
+      "101-150": "https://onlinetestpad.com/6f7cgcrtbj72w",
+      "151-200": "https://onlinetestpad.com/74aehbhhnzvwm",
+      "201-250": "https://onlinetestpad.com/ezhohruzoirme",
+      "251-300": "https://onlinetestpad.com/kck5rvs5l55lu",
+      "301-350": "https://onlinetestpad.com/5n66cj6sai7s2",
+      "351-400": "https://onlinetestpad.com/fvky3svqjyo5s",
+      "401-450": "https://onlinetestpad.com/42hcsjs63ikwa",
+      "451-500": "https://onlinetestpad.com/adl7pxokorb64",
+      "501-550": "https://onlinetestpad.com/vqdpksrzx56le",
+      "551++": "https://onlinetestpad.com/6xcq4uoqr5j7e"
+    },
+    "Chirurgie Nr. 1": {
+      "1-50": "https://onlinetestpad.com/x3dbdf3ou5ytw",
+      "51-100": "https://onlinetestpad.com/y3yrxavhfcg62",
+      "101-150": "https://onlinetestpad.com/tgroneptl2yes",
+      "151-200": "https://onlinetestpad.com/qowkg44x4yzf4",
+      "201++": "https://onlinetestpad.com/25ockpkzyavt2"
+    },
+    "Chirurgie Nr. 2": {
+      "1-50": "https://onlinetestpad.com/mk2ibmexy7ga2",
+      "51-100": "https://onlinetestpad.com/7545gxvynuya4",
+      "101-150": "https://onlinetestpad.com/bfhwweuthd2so",
+      "151-200": "https://onlinetestpad.com/b7i3dgy7k5any",
+      "201-250": "https://onlinetestpad.com/zfavtygpxh6m4",
+      "251++": "https://onlinetestpad.com/ejn6e2j4fryqk"
+    },
+    "Chirurgie pediatrică": {
+      "1-50": "https://onlinetestpad.com/2tutlxegspto4",
+      "51-100": "https://onlinetestpad.com/ien6d6lpzlujq",
+      "101++": "https://onlinetestpad.com/b6j5lhdklffyk"
+    },
+    "Chirurgie Nr. 3": {
+      "1-50": "https://onlinetestpad.com/sx5qxy3dmr4oe",
+      "51-100": "https://onlinetestpad.com/koux57ud6vgtw",
+      "101++": "https://onlinetestpad.com/dc66tzwcacmv6"
+    },
+    "Obstetrică și ginecologie": {
+      "1-50": "https://onlinetestpad.com/cc6fjxq7y5txk",
+      "51-100": "https://onlinetestpad.com/vgc4uuoj5fxn4",
+      "101-150": "https://onlinetestpad.com/6pxtzlyfzztee",
+      "151-200": "https://onlinetestpad.com/fnstjocfzovum",
+      "201-250": "https://onlinetestpad.com/airsfiq3res5e",
+      "251-300": "https://onlinetestpad.com/vijm7kx3y7uhe",
+      "301-350": "https://onlinetestpad.com/g34aqyb6y43s4",
+      "351-400": "https://onlinetestpad.com/5d7libl2zeuug",
+      "401++": "https://onlinetestpad.com/paptlxdeeqimy"
+    }
+  };
+  
+  // Creează butoanele pentru categorii
+  window.onload = function () {
+    const container = document.getElementById("category-container");
+  
+    Object.keys(DATA).forEach(cat => {
+      const btn = document.createElement("button");
+      btn.className = "button";
+      btn.textContent = cat;
+      btn.onclick = () => showSubcategories(cat);
+      container.appendChild(btn);
+    });
+  };
+  
+  function showSubcategories(category) {
+    const catDiv = document.getElementById("category-container");
+    const subDiv = document.getElementById("subcategory-container");
+    const title = document.getElementById("subcategory-title");
+    const list = document.getElementById("subcategory-list");
+  
+    title.textContent = category;
+    list.innerHTML = "";
+  
+    for (let key in DATA[category]) {
+      const li = document.createElement("li");
+      const a = document.createElement("a");
+      a.href = DATA[category][key];
+      a.textContent = key;
+      a.target = "_blank";
+      li.appendChild(a);
+      list.appendChild(li);
+    }
+  
+    catDiv.classList.add("hidden");
+    subDiv.classList.remove("hidden");
+  }
+  
+  function goBack() {
+    document.getElementById("subcategory-container").classList.add("hidden");
+    document.getElementById("category-container").classList.remove("hidden");
+  }
+  
