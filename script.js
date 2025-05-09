ae77aa13ec99001173b64511264e371de8339778
Gheorghii Rotaru <gheorghiirotaru13@gmail.com>
2025-05-09 12:20:02 +0300

    added for each group all tests section

--- script.js
+++ script.js
@@ -6,7 +6,8 @@
       "151-200": "https://onlinetestpad.com/llohvmxlso4b6",
       "201-250": "https://onlinetestpad.com/inb4rqilvefm4",
       "251-300": "https://onlinetestpad.com/cj3l3pvf4vuha",
-      "301++": "https://onlinetestpad.com/ok3b5ehisolt6"
+      "301++": "https://onlinetestpad.com/ok3b5ehisolt6",
+      "Toate": "https://onlinetestpad.com/77e5deddqrqfq"
     },
     "Cardiologie": {
       "1-50": "https://onlinetestpad.com/k5g43dru2hud2",
@@ -18,7 +19,8 @@
       "301-350": "https://onlinetestpad.com/sz3vvfzkxmnn6",
       "351-400": "https://onlinetestpad.com/zwwg2kbqsxvqk",
       "401-450": "https://onlinetestpad.com/c3e2ig5p53hay",
-      "451++": "https://onlinetestpad.com/m4x4wo2eh7tbc"
+      "451++": "https://onlinetestpad.com/m4x4wo2eh7tbc",
+      "Toate": "https://onlinetestpad.com/gllznthcxpqly"
     },
     "Gastroenterologie": {
       "1-50": "https://onlinetestpad.com/m73mt52ffwikk",
@@ -29,21 +31,24 @@
       "251-300": "https://onlinetestpad.com/ba6rz4nqu6lwy",
       "301-350": "https://onlinetestpad.com/bw2esbv22qpoe",
       "351-400": "https://onlinetestpad.com/3hq7vtmbnhbw2",
-      "401++": "https://onlinetestpad.com/rcsejxkrjou4s"
+      "401++": "https://onlinetestpad.com/rcsejxkrjou4s",
+      "Toate": "https://onlinetestpad.com/pknl6cmhv2fxg"
     },
     "Reumatologie": {
       "1-50": "https://onlinetestpad.com/pk3c2ix7jkva2",
       "51-100": "https://onlinetestpad.com/lmywe2z7k2gfq",
       "101-150": "https://onlinetestpad.com/a2ywqfe2cteja",
       "151-200": "https://onlinetestpad.com/2tladmxupgpic",
-      "201++": "https://onlinetestpad.com/ocm4ercjqvrs2"
+      "201++": "https://onlinetestpad.com/ocm4ercjqvrs2",
+      "Toate": "https://onlinetestpad.com/an7p7wblmruj4"
     },
     "Nefrologie": {
       "1-50": "https://onlinetestpad.com/6yvxtshwotzo4",
       "51-100": "https://onlinetestpad.com/zmq5oc5apphse",
       "101-150": "https://onlinetestpad.com/iqdkqbi7ewajg",
       "151-200": "https://onlinetestpad.com/63f4cn2nwve34",
-      "201++": "https://onlinetestpad.com/jvf74pbmgzxqm"
+      "201++": "https://onlinetestpad.com/jvf74pbmgzxqm",
+      "Toate": "https://onlinetestpad.com/47xpmardihxmy"
     },
     "Pediatrie": {
       "1-50": "https://onlinetestpad.com/e7ma63xlasahq",
@@ -57,14 +62,16 @@
       "401-450": "https://onlinetestpad.com/42hcsjs63ikwa",
       "451-500": "https://onlinetestpad.com/adl7pxokorb64",
       "501-550": "https://onlinetestpad.com/vqdpksrzx56le",
-      "551++": "https://onlinetestpad.com/6xcq4uoqr5j7e"
+      "551++": "https://onlinetestpad.com/6xcq4uoqr5j7e",
+      "Toate": "https://onlinetestpad.com/pu7lmavbylwca"
     },
     "Chirurgie Nr. 1": {
       "1-50": "https://onlinetestpad.com/x3dbdf3ou5ytw",
       "51-100": "https://onlinetestpad.com/y3yrxavhfcg62",
       "101-150": "https://onlinetestpad.com/tgroneptl2yes",
       "151-200": "https://onlinetestpad.com/qowkg44x4yzf4",
-      "201++": "https://onlinetestpad.com/25ockpkzyavt2"
+      "201++": "https://onlinetestpad.com/25ockpkzyavt2",
+      "Toate": "https://onlinetestpad.com/pzcajzh33rjey"
     },
     "Chirurgie Nr. 2": {
       "1-50": "https://onlinetestpad.com/mk2ibmexy7ga2",
@@ -72,17 +79,20 @@
       "101-150": "https://onlinetestpad.com/bfhwweuthd2so",
       "151-200": "https://onlinetestpad.com/b7i3dgy7k5any",
       "201-250": "https://onlinetestpad.com/zfavtygpxh6m4",
-      "251++": "https://onlinetestpad.com/ejn6e2j4fryqk"
+      "251++": "https://onlinetestpad.com/ejn6e2j4fryqk",
+      "Toate": "https://onlinetestpad.com/dryv3jt2cl35q"
     },
     "Chirurgie pediatrică": {
       "1-50": "https://onlinetestpad.com/2tutlxegspto4",
       "51-100": "https://onlinetestpad.com/ien6d6lpzlujq",
-      "101++": "https://onlinetestpad.com/b6j5lhdklffyk"
+      "101++": "https://onlinetestpad.com/b6j5lhdklffyk",
+      "Toate": "https://onlinetestpad.com/uwz4tntt7ndwk"
     },
     "Chirurgie Nr. 3": {
       "1-50": "https://onlinetestpad.com/sx5qxy3dmr4oe",
       "51-100": "https://onlinetestpad.com/koux57ud6vgtw",
-      "101++": "https://onlinetestpad.com/dc66tzwcacmv6"
+      "101++": "https://onlinetestpad.com/dc66tzwcacmv6",
+      "Toate": "https://onlinetestpad.com/pyqms2ffr35bq"
     },
     "Obstetrică și ginecologie": {
       "1-50": "https://onlinetestpad.com/cc6fjxq7y5txk",
@@ -93,7 +103,8 @@
       "251-300": "https://onlinetestpad.com/vijm7kx3y7uhe",
       "301-350": "https://onlinetestpad.com/g34aqyb6y43s4",
       "351-400": "https://onlinetestpad.com/5d7libl2zeuug",
-      "401++": "https://onlinetestpad.com/paptlxdeeqimy"
+      "401++": "https://onlinetestpad.com/paptlxdeeqimy",
+      "Toate": "https://onlinetestpad.com/ub7qgpjucyqvi"
     }
   };
   
